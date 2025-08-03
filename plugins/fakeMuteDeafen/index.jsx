ebcbf7ebe1571490f319e65506db06c26c6d6034
Gust Who? <lucgustga@outlook.com>
2025-08-03 16:01:58 +0800

    who the fuck coded ts

--- plugins/fakeMuteDeafen/index.jsx
+++ plugins/fakeMuteDeafen/index.jsx
@@ -14,7 +14,7 @@ function createButton() {
 		button.classList.add("guhw-fmd")
 		button.childNodes[0].setAttribute("aria-label","Lock Voice State")
 		button.querySelector("svg").innerHTML = `<path fill="currentColor" fill-rule="evenodd" d="M6 9h1V6a5 5 0 0 1 10 0v3h1a3 3 0 0 1 3 3v8a3 3 0 0 1-3 3H6a3 3 0 0 1-3-3v-8a3 3 0 0 1 3-3Zm9-3v3H9V6a3 3 0 1 1 6 0Zm-1 8a2 2 0 0 1-1 1.73V18a1 1 0 1 1-2 0v-2.27A2 2 0 1 1 14 14Z" clip-rule="evenodd" class=""></path>`
-		button.addEventListener("click",window.fakeDeaf())
+		button.addEventListener("click",window.fakeDeaf)
 	}
 	return button
 }
